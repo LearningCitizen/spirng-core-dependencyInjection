822c15e0559f315623baaf44b48a0505cdf5d945
hippolyt <john-son.hippolyte-ext@amundi.com>
2019-09-26 16:49:02 +0200

    add spring annotations for dependency injection

--- src/main/java/com/learningcitizen/spring/MyApplication.java
+++ src/main/java/com/learningcitizen/spring/MyApplication.java
@@ -1,9 +1,14 @@
 package com.learningcitizen.spring;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MyApplication {
 
     MessageService service;
 
+    @Autowired
     public void setService(MessageService service)
     {
         this.service = service;
