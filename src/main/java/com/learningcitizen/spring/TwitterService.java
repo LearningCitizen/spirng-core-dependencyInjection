f43f1fad60b7550036b689511f06a5e432b0ca3a
hippolyt <john-son.hippolyte-ext@amundi.com>
2019-10-02 16:19:05 +0200

    TwitterServce implements MessageService

--- src/main/java/com/learningcitizen/spring/TwitterService.java
+++ src/main/java/com/learningcitizen/spring/TwitterService.java
@@ -1,6 +1,6 @@
 package com.learningcitizen.spring;
 
-public class TwitterService {
+public class TwitterService implements MessageService{
     public boolean sendMessage (String msg, String rec)
     {
         System.out.println("send twitt : "+msg+" to: "+rec);
